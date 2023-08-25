2bbe9d6713d8719a9ef369e245056905106acb9a
Ali Ramazanov <aliasifzade@gmail.com>
2023-08-25 12:03:11 +0400

    Significant Updates

--- src/api.js
+++ src/api.js
@@ -2,11 +2,13 @@ export const fetchData = async () => {
   try {
     const response = await fetch("/data/data.JSON");
     if (!response.ok) {
-      throw new Error("Something went wrong...");
+      throw new Error(
+        `Something went wrong (Status ${response.status}): ${response.statusText}`
+      );
     }
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
-    throw new Error("Error when fetching: " + error);
+    throw new Error(`Error when fetching data: ${error.message}`);
   }
 };
-
